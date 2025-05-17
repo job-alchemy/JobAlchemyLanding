3f94f613350997f34287b3ace492ba709c8ae7a6
niranjan.rakesh <niranjan@jobalchemy.ai>
2025-05-17 18:11:53 +0400

    refactor: comment out unused Button import in Navbar for potential future use

--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from 'next/link'
-import { Button } from '../ui/button'
+// import { Button } from '../ui/button'
 import { useState } from 'react';
 import { ThemeToggle } from '../theme/ThemeToggle';
 
